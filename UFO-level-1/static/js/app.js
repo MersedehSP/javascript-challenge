9a231ff041891720761d9981237b5c7d0e02ae3d
Mersedeh Saniepay <mersedeh.saniepay@gmail.com>
2020-12-17 21:06:20 -0500

    part 1 almost

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,11 +2,12 @@
 var tableData = data;
 console.log(tableData);
 
-// YOUR CODE HERE!
+// YOUR CODE HERE!\\
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // // Step 1: Loop Through UFO Sghtings data (tableData) and console.log each column
+
 tableData.forEach(function(ufoSightings) {
   console.log(ufoSightings);
  
@@ -17,11 +18,45 @@ tableData.forEach(function(ufoSightings) {
 
 
 // // Step 3:  Use `Object.entries` to console.log each UFO Sghtings value
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
 
    Object.entries(ufoSightings).forEach(function([key, value]) {
      console.log(key, value);
   });
  });
+
+ ////////////////// Select the  button///////////////////////////////////////////////
+
+var button = d3.select(".button");
+
+// Use D3 `.on` to attach a click handler for the button
+button.on("click", function() {
+    
+  // Prevent the page from refreshing////////////////////////
+  // d3.event.preventDefault();
+    tbody.html("");
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    
+    //filter data
+    var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
+
+    console.log(filteredData);
+    
+    ///////////////
+
+
+    filteredData.forEach(function(selected) {
+        console.log(selected); 
+        var row = tbody.append("tr");
+        Object.entries(selected).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});    
