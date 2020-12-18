5d203fd078b7e3cf2729e0d94b2d69566ca8c74f
Mersedeh Saniepay <mersedeh.saniepay@gmail.com>
2020-12-18 16:31:45 -0500

    part two done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,35 +5,33 @@ console.log(tableData);
 // YOUR CODE HERE!\\
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+function ufoTable(data) {
+  tbody.html("");
+  data.forEach(function(selected) {
+    console.log(selected); 
+    var row = tbody.append("tr");
+    Object.entries(selected).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+}
+////
+ufoTable(tableData);
 
-// // Step 1: Loop Through UFO Sghtings data (tableData) and console.log each column
-
-tableData.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
- 
-
-// // Step 2:  Use d3 to append one table row `tr` for each UFO Sghtings object
-
-   var row = tbody.append("tr");
-
-
-// // Step 3:  Use `Object.entries` to console.log each UFO Sghtings value
-
-   Object.entries(ufoSightings).forEach(function([key, value]) {
-     console.log(key, value);
-  });
- });
 
  ////////////////// Select the  button///////////////////////////////////////////////
 
-var button = d3.select(".button");
+var button = d3.select("#filter-btn");
 
-// Use D3 `.on` to attach a click handler for the button
+//  Use D3 `.on` to attach a click handler for the button
 button.on("click", function() {
     
-  // Prevent the page from refreshing////////////////////////
-  // d3.event.preventDefault();
+  // Prevent the page from refreshing
+    d3.event.preventDefault();
     tbody.html("");
+
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
 
@@ -49,14 +47,6 @@ button.on("click", function() {
     
     ///////////////
 
+  ufoTable(filteredData);
 
-    filteredData.forEach(function(selected) {
-        console.log(selected); 
-        var row = tbody.append("tr");
-        Object.entries(selected).forEach(function([key, value]) {
-            console.log(key, value);
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
 });    
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,70 @@
 // from data.js
 var tableData = data;
+var filterObject = {};
 
 // YOUR CODE HERE!
+
+console.log(tableData);
+
+// YOUR CODE HERE!\\
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+function ufoTable(data) {
+  tbody.html("");
+  data.forEach(function(selected) {
+    console.log(selected); 
+    var row = tbody.append("tr");
+    Object.entries(selected).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+}
+////
+ufoTable(tableData);
+
+function inputFilter(){
+    var changedElement = d3.select(this).select("input");
+    var elementValue = changedElement.property("value");
+    var id = changedElement.attr("id");
+
+    if (elementValue)
+    {
+        filterObject[id] = elementValue;
+    
+    }
+    else {
+        delete filterObject[id];
+    }
+    filterTable();
+}
+
+function filterTable(){
+
+
+ //////////////// Select the  button///////////////////////////////////////////////
+
+
+    
+  
+    
+    Object.entries(filterObject).forEach(([key, value])=>{
+     var   filteredData = tableData.filter(row => row[key]=== value);
+    
+
+    
+    
+  
+
+  ufoTable(filteredData);
+
+});    
+
+}
+
+
+
+
+//  Use D3 `.on` to attach a click handler for the button
+d3.selectAll(".filter").on("change", inputFilter);
